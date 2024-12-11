4213abfd5b4fc39a75771f1bb28298bd7c896027
rianaditro <rianaditro@gmail.com>
2024-12-11 11:21:42 +0700

    using array to monitor data

--- main.py
+++ main.py
@@ -1,38 +1,58 @@
-import time, os
+import time, json, os, argparse
 
 from spider.crawler import Crawler
 
-REPORT_CODES = {
-    "Neraca_Keuangan": "PGWS-908-00021",
-    "Laba_Rugi": "PGWS-908-00022",
-    "Rasio_Keuangan": "PGWS-908-00027",
-}
-MONTHS = [3, 6, 9, 12]
-YEARS = [i for i in range(2010, 2024)]
-BANK_NAMES = ['002-PT BANK RAKYAT INDONESIA (PERSERO), Tbk', '003-PT BANK EKSPOR INDONESIA (PERSERO)', '008-PT BANK MANDIRI (PERSERO)', '009-PT BANK NEGARA INDONESIA (PERSERO), Tbk ', '200-PT BANK TABUNGAN NEGARA (PERSERO), Tbk', '026-PT.LIPPOBANK TBK', '060-PT.RABO BANK DUTA INDONESIA', '110-PT BPD JAWA BARAT DAN BANTEN, Tbk', '111-PT BPD DKI', '112-PT BPD DAERAH ISTIMEWA YOGYAKARTA', '113-PT BPD JAWA TENGAH', '114-PT BPD JAWA TIMUR Tbk', '115-PT BPD JAMBI ', '116-PT BANK ACEH', '117-PT BPD SUMATERA UTARA ', '118-PT BANK NAGARI', '119-PT BPD RIAU DAN KEPULAUAN RIAU ', '120-PT BPD SUMATERA SELATAN DAN BANGKA BELITUNG', '121-PT BPD LAMPUNG', '122-PT BPD KALIMANTAN SELATAN', '123-BPD KALIMANTAN BARAT', '124-PT BPD KALIMANTAN TIMUR DAN KALIMANTAN UTARA', '125-PT BPD KALIMANTAN TENGAH', '126-PT BPD SULAWESI SELATAN DAN SULAWESI BARAT ', '127-PT BPD SULAWESI UTARA GORONTALO', '128-PT BPD NUSA TENGGARA BARAT', '129-PT BPD BALI ', '130-PT BPD NUSA TENGGARA TIMUR ', '131-PT BPD MALUKU DAN MALUKU UTARA', '132-PT BPD PAPUA', '133-PT BPD BENGKULU', '134-PT.  BPD SULAWESI TENGAH', '135-PT BPD SULAWESI TENGGARA', '137-PT BANK PEMBANGUNAN DAERAH BANTEN, Tbk (d.h SANDI 558-BANK PUNDI )', '140-BANK CITRA MAKMUR ASIA (eks.YAMA BANK)', '011-PT BANK DANAMON INDONESIA, Tbk ', '013-PT BANK PERMATA, Tbk', '014-PT BANK CENTRAL ASIA, Tbk', '016-PT BANK MAYBANK INDONESIA, Tbk', '019-PT PAN INDONESIA BANK, Tbk ', '020-PT BANK ARTA NIAGA KENCANA', '022-PT BANK CIMB NIAGA, Tbk', '023-PT BANK UOB INDONESIA', '027-PT PRIMA EXPRESS BANK', '028-PT BANK OCBC NISP, Tbk', '034-PT ING INDONESIA BANK', '035-PT BANK SOCIETE GENERALE INDONESIA', '036-PT BANK CHINA CONSTRUCTION BANK INDONESIA, Tbk', '037-PT BANK ARTHA GRAHA INTERNASIONAL, Tbk', '038-PT BANK PARIBAS - BBD INDONESIA', '039-PT. BANK CREDIT AGRICOLE INDOSUEZ', '044-PT BANK IBJ INDONESIA', '045-PT BANK SUMITOMO INDONESIA ', '046-PT BANK DBS INDONESIA ', '047-PT BANK RESONA PERDANIA', '048-PT BANK MIZUHO INDONESIA', '049-PT. BANK UFJ INDONESIA', '053-PT.KEPPEL TAT LEE BUANA BANK', '054-PT BANK CAPITAL INDONESIA, Tbk', '055-PT BANK SAKURA SWADHARMA', '056-PT TOKAI LIPPO BANK', '057-PT BANK BNP PARIBAS INDONESIA', '059-PT BANK KEB INDONESIA', '061-PT BANK ANZ INDONESIA', '062-PT BANK DAI-ICHI KANGYO INDONESIA', '068-PT BANK WOORI INDONESIA', '072-PT BANK DAGANG BALI', '073-PT BANK UNIVERSAL Tbk.', '075-PT BANK UNIBANK Tbk', '076-PT BANK BUMI ARTA, Tbk', '085-PT BANK ARTHA GRAHA', '087-PT BANK HSBC INDONESIA', '088-PT BANK ANTARDAERAH', '089-PT BANK INTERIM INDONESIA', '093-PT BANK IFI', '095-PT BANK JTRUST INDONESIA, TBK', '097-PT BANK MAYAPADA INTERNATIONAL, Tbk', '145-PT BANK NUSANTARA PARAHYANGAN,Tbk', '146-PT BANK OF INDIA INDONESIA, Tbk', '151-PT BANK MESTIKA DHARMA ', '152-PT BANK SHINHAN INDONESIA', '153-PT BANK SINARMAS, Tbk', '157-PT BANK MASPION INDONESIA ', '158-PT ARTAMEDIA BANK', '159-PT BANK HAGAKITA', '161-PT BANK GANESHA ', '162-PT BANK WINDU KENTJANA', '164-PT BANK ICBC INDONESIA', '166-PT BANK HARMONI INTERNATIONAL', '167-PT BANK QNB INDONESIA, Tbk', '168-PT BANK PIKKO Tbk', '212-PT BANK WOORI SAUDARA INDONESIA 1906, Tbk', '213-PT BANK SMBC INDONESIA, TBK (PT BANK BTPN, Tbk)', '332-PT.BANK JAKARTA', '369-PT BANK PATRIOT', '405-PT BANK SWAGUNA ', '422-PT. BANK JASA ARTA', '426-PT BANK MEGA, Tbk ', '441-PT BANK KB BUKOPIN, Tbk', '459-PT BANK BISNIS INTERNASIONAL ', '466-PT BANK OKE INDONESIA', '472-PT BANK JASA JAKARTA ', '484-PT BANK KEB HANA INDONESIA', '485-PT BANK MNC INTERNASIONAL, Tbk', '490-PT BANK NEO COMMERCE TBK', '491-PT BANK MITRANIAGA', '494-PT BANK RAYA INDONESIA TBK', '498-PT BANK SBI INDONESIA ', '501-PT BANK DIGITAL BCA', '503-PT BANK NATIONALNOBU', '504-PT BANK ASIATIC', '506-PT BANK UMUM TUGU', '513-PT BANK INA PERDANA ', '520-PT PRIMA MASTER BANK ', '521-PT BANK PERSYARIKATAN INDONESIA ', '523-PT BANK SAHABAT SAMPOERNA', '526-PT BANK OKE INDONESIA TBK', '531-PT BANK AMAR INDONESIA', '532-PT BANK PRASIDHA UTAMA', '533-PT BANK DANPAC', '535-PT BANK SEABANK INDONESIA', '542-PT BANK JAGO Tbk', '546-PT GLOBAL INTERNATIONAL BANK', '547-PT BANK SAHABAT PURBA DANARTA', '548-PT BANK MULTIARTA SENTOSA ', '552-PT BANK RATU', '553-PT BANK HIBANK INDONESIA', '555-PT BANK INDEX SELINDO ', '558-PT BANK PEMBANGUNAN DAERAH BANTEN, TBK', '559-PT CENTRATAMA NASIONAL BANK', '562-PT Super Bank Indonesia', '564-PT BANK MANDIRI TASPEN', '566-PT BANK VICTORIA INTERNATIONAL, Tbk', '567-PT ALLO BANK INDONESIA Tbk', '945-PT BANK IBK INDONESIA Tbk', '946-PT BANK MERINCORP', '949-PT BANK CTBC INDONESIA', '950-PT BANK COMMONWEALTH ', '030-AMERICAN EXPRESS BANK LTD.', '031-CITIBANK NA ', '032-JP. MORGAN CHASE BANK, N.A.', '033-BANK OF AMERICA, N.A ', '040-THE BANGKOK BANK COMP. LTD', '041-THE HONGKONG AND SHANGHAI BANKING CORP', '042-MUFG Bank, Ltd', '050-STANDARD CHARTERED BANK ', '052-THE ROYAL BANK OF SCOTLAND N.V.', '067-DEUTSCHE BANK AG. ', '069-BANK OF CHINA (HONGKONG) LIMITED']
+
+def validator_reader(filename:str, mode:str):
+    with open(filename, "r") as file:
+        json_data = json.load(file)
+    
+    # for data_per_mode in json_data:
+    #     if data_per_mode["mode"] == mode:
+    #         return data_per_mode["data"]
+    return json_data
+
+def browser_loop(mode:str, filename:str):
+    data = validator_reader(filename, mode)
+    max = len(data)
+
+    crawler = Crawler(mode)
+
+    for i, d in enumerate(data):
+        print(f"Processing {i} of {max}")
+        try:
+            
+            if d["status"] == "downloaded" or d["status"] == "not_exist":
+                continue
+            else:
+                if d["status"] == "corrupted":
+                    os.remove(f"{crawler.download_folder}/{d['filename']}")
+                    print(f"delete {d['filename']}")
+
+                while True:
+                    updated_status = crawler.get_excel(d)
+
+                    if updated_status == "downloaded" or updated_status == "not_exist":
+                        d["status"] = updated_status
+                        break
+                    elif updated_status == "object_reference_error" or updated_status == "download_incomplete":
+                        crawler = Crawler(mode)
+        except:
+            with open(f"validator_{mode}.json", "w") as fp:
+                json.dump(data, fp, indent=4)
+
+    with open(f"validator_{mode}.json", "w") as fp:
+                json.dump(data, fp, indent=4)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--mode", type=str, help="['neraca', 'laba_rugi', 'rasio']")
+    parser.add_argument("-f", "--filename", type=str, help="JSON filename")
+
+    args = parser.parse_args()
+
+    browser_loop(args.mode, args.filename)
 
 
 if __name__ == "__main__":
-    crawler = Crawler()
-
-    for bank in BANK_NAMES:
-        for year in YEARS:
-            for month in MONTHS:
-                for report_code in REPORT_CODES.values():
-                    filename = f"{bank}_{year}_{month}_{report_code}.xlsx"
-                    excel = crawler.get_excel(bank, year, month, report_code)
-                    
-                    if excel:
-                        files = os.listdir("downloaded_files")
-
-                        while True:
-                            time.sleep(2)
-                            if len(files) < len(os.listdir("downloaded_files")):
-                                for file in os.listdir("downloaded_files"):
-                                    if file not in files:
-                                        os.rename(f"downloaded_files/{file}", f"downloaded_files/{filename}")
-                                break
-                            else:
-                                print("Waiting for file to download")
-
-                raise StopIteration
+    main()
--- spider/crawler.py
+++ spider/crawler.py
@@ -1,66 +1,91 @@
+import os
 import time
-from urllib.parse import quote
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException as SERE
+from selenium.common.exceptions import WebDriverException as WDE
 
 
 class Crawler:
-    def __init__(self):
-        self.base_url = "https://ojk.go.id/id/kanal/perbankan/data-dan-statistik/laporan-keuangan-perbankan/Default.aspx"
-        self.frame_url = "https://cfs.ojk.go.id/cfs/ReportViewerForm.aspx?"
-
+    def __init__(self, mode:str):
         self.options = Options()
-        # self.options.add_argument("--headless")
-        self.options.add_argument("--start-maximized")
+        self.options.add_argument("--headless")
+        # self.options.add_argument("--start-maximized")
 
         prefs = {"profile.default_content_settings.popups": 0,    
-        "download.default_directory":r"/home/rianaditro/projects/data_scraping/ojk/downloaded_files",
+        "download.default_directory":fr"/home/rianaditro/projects/data_scraping/ojk/{mode}",
         "download.prompt_for_download": False, 
         "download.directory_upgrade": True}
 
         self.options.add_experimental_option("prefs", prefs)
+        self.download_folder = mode
 
         self.driver = webdriver.Chrome(options=self.options)
         self.wait = WebDriverWait(self.driver, 10)
+    
+    def restart_driver(self):
+        if self.driver:
+            try:
+                print("Driver restart")
+                self.driver.quit()
+            except WDE as e:
+                print(f"Error quitting WebDriver: {e}")
+            finally:
+                self.driver = webdriver.Chrome(options=self.options)
+                self.wait = WebDriverWait(self.driver, 10)
+    
+    def monitoring_download(self, filename:str):
+        existing_files = set(os.listdir(self.download_folder))
+        wait_time = 1
+        status_code = 0
 
+        while True:
+            print("waiting download")
+            time.sleep(wait_time)
+            wait_time += 1
 
-    def _find_until_clickable(self, ID:str):
+            current_files = set(os.listdir(self.download_folder))
+            new_file = current_files - existing_files
+
+            if new_file: # DEBUG: This should be only one
+                os.rename(f"{self.download_folder}/{list(new_file)[0]}", f"{self.download_folder}/{filename}")
+                print(f"{filename} downloaded")
+                status_code = 1
+                return status_code
+
+            if wait_time > 10:
+                print(f"Download time exceed: {self.driver.current_url}")
+                break
+        return status_code
+    
+    def find_until_clickable(self, ID:str):
         return self.wait.until(
             EC.element_to_be_clickable((By.ID, ID))
         )
 
-    def _get_valid_url(self, bank:str, year:int, month:int, report_code:str)->str:
-        """Convert input to valid url"""
-        # Split bank input
-        bank_code, bank_name = bank.split("-", 1) # Max split is 1
-        bank_name = bank_name.replace(" ", "+").replace(",", "%2C")
-
-        # Change the report code for 2020+
-        if year > 2020:
-            report_code = report_code+"A"
-
-        return f"{self.frame_url}BankCodeNumber={bank_code}&BankCode={bank_name}&Month={month}&Year={year}&FinancialReportPeriodTypeCode=B&FinancialReportTypeCode={report_code}"
-
-    def get_excel(self, bank:str, year:int, month:int, report_code:str):
+    def get_excel(self, data:dict):
         """Opening the url and clicking the excel download button"""
-        url = self._get_valid_url(bank, year, month, report_code)        
+        url = data['file_url']
+        filename = data['filename']
+
         self.driver.get(url)
+        print(f"Opening {url}")
 
-        try:
-            self._find_until_clickable("CFSReportViewer_ctl05_ctl04_ctl00_ButtonLink").click()
+        if "object" in self.driver.title:
+            return "object_reference_error"
+        elif "tidak tersedia" in self.driver.page_source:
+            print(f"Laporan Tidak Tersedia: {url}")
+            return "not_exist"
+        else:
+            self.find_until_clickable("CFSReportViewer_ctl05_ctl04_ctl00_ButtonLink").click()
             self.driver.find_element(By.LINK_TEXT, "Excel").click()
-            return True
+            status_code = self.monitoring_download(filename)
 
-        except Exception as e:
-            if "Tidak Tersedia" not in self.driver.page_source:
-                print("Laporan Tidak Tersedia")
+            if status_code:
+                return "downloaded"
             else:
-                print(e)
-            return False
-
+                return "download_incomplete"
